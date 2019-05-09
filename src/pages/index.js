456732d4bd0af33bcb366fa31f73eda951c04825
David Poindexter <drpoindexter@gmail.com>
2019-05-09 16:49:37 -0400

    Add donation info

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <h1>St Jude Rocks</h1>
+    <p>Support the awesome St Jude drive</p>
+    <p><a href="https://tiltify.com/@quiltoni/quiltoni-2019-playlive-campaign" target="_blank">Now go build something great.</a></p>
+    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
-    </div>
+    </div> */}
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
